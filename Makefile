a16ca01719a7008227faf54995d2f2d762de958c
Junsious <114828272+Junsious@users.noreply.github.com>
2024-09-09 22:06:34 +0300

    Add files via upload

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ OSNAME := ZenOS
 ISOFILE := $(OSNAME).iso
 BINARY := $(OSNAME).bin
 
-# Указываем компилятор и ассемблер
+# Указываем компиляторы и ассемблер
 AS := i386-elf-as
 CC := i386-elf-gcc
 
@@ -12,43 +12,61 @@ CFLAGS := -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 LDFLAGS := -ffreestanding -O2 -nostdlib
 
 # Файлы
-KERNEL_SRC := kernel.c
+KERNEL_C_SRC := kernel.c
+LIB_C_SRC := lib.c
 BOOT_SRC := boot.asm
 LINKER_SCRIPT := linker.ld
 GRUB_CFG := grub.cfg
 
 # Объектные файлы
-OBJS := boot.o kernel.o
+OBJS := boot.o kernel.o lib.o
 
 # Директории
 ISO_DIR := isodir
 BOOT_DIR := $(ISO_DIR)/boot
 GRUB_DIR := $(BOOT_DIR)/grub
 
+# Прогресс-бар цвета
+define print_progress
+	@printf "\033[1;32m[ %-20s ]\033[0m\r" $(1)
+endef
+
 # Цель по умолчанию
 all: $(ISOFILE)
 
 # Сборка ISO
 $(ISOFILE): $(BINARY) $(GRUB_CFG)
+	@$(call print_progress, "ISO Creation")
 	mkdir -p $(GRUB_DIR)
 	cp $(BINARY) $(BOOT_DIR)/$(BINARY)
 	cp $(GRUB_CFG) $(GRUB_DIR)/grub.cfg
 	grub-mkrescue -o $(ISOFILE) $(ISO_DIR)
+	@echo "ISO created successfully"
 
 # Сборка бинарного файла
 $(BINARY): $(OBJS) $(LINKER_SCRIPT)
+	@$(call print_progress, "Linking")
 	$(CC) -T $(LINKER_SCRIPT) -o $(BINARY) $(LDFLAGS) $(OBJS) -lgcc
 	grub-file --is-x86-multiboot $(BINARY)
+	@echo "Binary created successfully"
 
 # Сборка объектных файлов
 boot.o: $(BOOT_SRC)
+	@$(call print_progress, "Assembling boot.asm")
 	$(AS) $(BOOT_SRC) -o boot.o
 
-kernel.o: $(KERNEL_SRC)
-	$(CC) -c $(KERNEL_SRC) -o kernel.o $(CFLAGS)
+kernel.o: $(KERNEL_C_SRC)
+	@$(call print_progress, "Compiling kernel.c")
+	$(CC) -c $(KERNEL_C_SRC) -o kernel.o $(CFLAGS)
+
+lib.o: $(LIB_C_SRC)
+	@$(call print_progress, "Compiling lib.c")
+	$(CC) -c $(LIB_C_SRC) -o lib.o $(CFLAGS)
 
 # Очистка
 clean:
+	@echo "Cleaning up..."
 	rm -rf $(OBJS) $(BINARY) $(ISOFILE) $(ISO_DIR)
+	@echo "Cleaned successfully"
 
 .PHONY: all clean
--- kernel.c
+++ kernel.c
@@ -2,95 +2,28 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#if defined(__linux__)
-#error "You are not using a cross-compiler"
-#endif
-
-#if !defined(__i386__)
-#error "This kernel must be compiled with a ix86-elf compiler"
-#endif
-
-enum vga_color {
-    VGA_COLOR_BLACK = 0,
-    VGA_COLOR_BLUE = 1,
-    VGA_COLOR_GREEN = 2,
-    VGA_COLOR_CYAN = 3,
-    VGA_COLOR_RED = 4,
-    VGA_COLOR_MAGENTA = 5,
-    VGA_COLOR_BROWN = 6,
-    VGA_COLOR_LIGHT_GREY = 7,
-    VGA_COLOR_DARK_GREY = 8,
-    VGA_COLOR_LIGHT_BLUE = 9,
-    VGA_COLOR_LIGHT_GREEN = 10,
-    VGA_COLOR_LIGHT_CYAN = 11,
-    VGA_COLOR_LIGHT_RED = 12,
-    VGA_COLOR_LIGHT_MAGENTA = 13,
-    VGA_COLOR_LIGHT_BROWN = 14,
-    VGA_COLOR_WHITE = 15,
-};
-
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
-    return fg | (bg << 4);
-}
-
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
-    return (uint16_t) uc | ((uint16_t) color << 8);
-}
-
-size_t strlen(const char* str) {
-    size_t len = 0;
-    while (str[len])
-        len++;
-    return len;
-}
-
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
-
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
-uint16_t* terminal_buffer;
-
-void terminal_initialize(void) {
-    terminal_row = 0;
-    terminal_column = 0;
-    terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-    terminal_buffer = (uint16_t*) 0xB8000;
-
-    for (size_t y = 0; y < VGA_HEIGHT; y++) {
-        for (size_t x = 0; x < VGA_WIDTH; x++) {
-            const size_t index = y * VGA_WIDTH + x;
-            terminal_buffer[index] = vga_entry(' ', terminal_color);
-        }
-    }
-}
-
-void terminal_putchar(char c) {
-    if (c == '\n') {
-        terminal_column = 0;
-        if (++terminal_row == VGA_HEIGHT) {
-            terminal_row = 0;
-        }
-    } else {
-        terminal_buffer[terminal_row * VGA_WIDTH + terminal_column] = vga_entry(c, terminal_color);
-        if (++terminal_column == VGA_WIDTH) {
-            terminal_column = 0;
-            if (++terminal_row == VGA_HEIGHT) {
-                terminal_row = 0;
-            }
-        }
-    }
-}
-
-void terminal_writestring(const char* data) {
-    size_t len = strlen(data);
-    for (size_t i = 0; i < len; i++) {
-        terminal_putchar(data[i]);
-    }
-}
+extern void terminal_initialize(void);
+extern void terminal_writestring(const char* data);
+extern void terminal_readstring(char* buffer, size_t max_length);
+extern void handle_input(const char* input);
 
 void kernel_main(void) {
     terminal_initialize();
-    terminal_writestring("Welcome to ZenOS!");
+    terminal_writestring("Welcome to ZenOS!\n");
+    terminal_writestring("Press any key to continue...\n");
+
+    // Ожидание нажатия клавиши
+    char wait_key = ' ';
+    while (wait_key == ' ') {
+        // Здесь нужно добавить код для ожидания нажатия клавиши
+        // Для примера, будем использовать заглушку
+        wait_key = ' '; // Замените это на код для реального получения символа
+    }
+
+    terminal_writestring("ZenOS> ");
+    char input_buffer[128];
+    while (true) {
+        terminal_readstring(input_buffer, sizeof(input_buffer));
+        handle_input(input_buffer);
+    }
 }
